13f5c80e24e3be4ccec4790aa9072f4eb6535dfc
suvii22 <45846919+suvii22@users.noreply.github.com>
2019-12-23 23:29:42 +0800

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -9,22 +9,22 @@ void swap(int *i,int *j)
  
 void HeapAdjust(int arr[],int i,int nlen)
 {
-	int nchild,ntmp;
-	for(ntmp=arr[i];2*i+1<nlen;i=nchild)
-	{
-		nchild=2*i+1;
-		if (nchild != nlen - 1 && arr[nchild + 1] > arr[nchild])
-            ++nchild;
-        if (ntmp < arr[nchild])
-        {
-            arr[i] = arr[nchild];
-        }
-        else 
-        {
-            break;
-        }
-    }
-    arr[i] = ntmp;
+     int nchild,ntmp;
+     for(ntmp=arr[i];2*i+1<nlen;i=nchild)
+     {
+         nchild=2*i+1;
+         if (nchild != nlen - 1 && arr[nchild + 1] > arr[nchild])
+             ++nchild;
+         if (ntmp < arr[nchild])
+         {
+             arr[i] = arr[nchild];
+         }
+         else 
+         {
+             break;
+         }
+     }
+     arr[i] = ntmp;
 	
 	
 }
@@ -33,20 +33,20 @@ void HeapSort(int arr[],int len)
 	int i;
 	for(i=len/2-1;i>=0;i--)
 	{
-		HeapAdjust(arr,i,len);
+	    HeapAdjust(arr,i,len);
 	}
 	for(i=len-1;i>0;i--)
 	{
-		swap(&arr[0],&arr[i]);
-		HeapAdjust(arr,0,i);
+	    swap(&arr[0],&arr[i]);
+	    HeapAdjust(arr,0,i);
 	}
 	
 } 
 int main()
 {
-	int i;
+    int i;
     int length = sizeof(heapBuf) / sizeof(int);
-	printf("%d\n",length);  
+    printf("%d\n",length);  
     /* sort */  
     HeapSort(heapBuf,length); 
     for(i=0;i<length;i++) printf("%d ",heapBuf[i]);
